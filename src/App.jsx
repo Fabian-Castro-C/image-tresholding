175733b8a757154ce5b1884284e6e9b16af4b862
fabian-castro-c <fabian.castro.c@ug.uchile.cl>
2025-01-28 20:51:29 -0300

    feat: integrate file loading and error handling in App component with CanvasImage rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useMemo } from "react"
+import CanvasImage from "./components/CanvasImage"
+import useFileLoader from "./hooks/useFileLoader"
+
+const IMAGE_PATH = "../public/example.txt"
+
 function App() {
+  const { parsedData, error, loadFile } = useFileLoader()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await loadFile(IMAGE_PATH)
+    }
+
+    fetchData()
+  }, [loadFile])
+
+  const colormap = useMemo(() => (value) => {
+    const gray = Math.floor(value * 255)
+    return [gray, gray, gray, 255]
+  }, [])
+
   return (
     <>
-      <h1>Image Tresholding App</h1>
+      <h1>Image Thresholding App</h1>
+      {error && <p>Error: {error}</p>}
+      {parsedData && <CanvasImage 
+        data={parsedData.data} 
+        metadata={parsedData.metadata} 
+        colormap={colormap}
+      />}
     </>
   )
 }
